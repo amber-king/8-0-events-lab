672817371a01ee190dfe576e0d3ed99bfac7447e
Amber King <amber.king9292@gmail.com>
2022-12-15 14:38:09 -0500

    Events-Lab_Done

--- main.js
+++ main.js
@@ -7,3 +7,31 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+// Change the background of current-color---v
+const colorScale = document.querySelectorAll(".color")
+
+colorScale.forEach(colorsDiv => {
+  colorsDiv.addEventListener('click', buttonClicked = () => {
+    const afterColor = colorsDiv.style.background
+    const presentColor = document.getElementById("current-color")
+    presentColor.style.background = afterColor
+  })
+})
+
+// Change cells to the currently selected color --v
+const neededCells = document.querySelectorAll("#canvas .cell")
+
+neededCells.forEach( cell => {
+  cell.addEventListener('click', buttonCLicked = () => {
+    cell.style.background = document.getElementById("current-color").style.background
+  })
+})
+
+// Reset Buttons, make the default white--v
+// make in HTML button data to run through properly!
+const resetClick = document.querySelector(".reset")
+
+resetClick.addEventListener('click', () => {
+  neededCells.forEach( cell => cell.style.background = "white")
+})
